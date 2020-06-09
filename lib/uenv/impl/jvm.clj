2110a793eaee9374307b2b8d3817ccba782b5251
Matti Hänninen <matti@mjhanninen.com>
2020-06-09 22:15:19 +0300

    Load .env file only if it exists

--- lib/uenv/impl/jvm.clj
+++ lib/uenv/impl/jvm.clj
@@ -8,12 +8,13 @@
 
 (defn source-env-file
   [->env-file-event path]
-  (let [f (io/as-file path)
-        p (.getAbsolutePath f)]
-    (with-open [r (io/reader f)]
-      (mapv #(->env-file-event p (inc %2) %1)
-            (line-seq r)
-            (range)))))
+  (let [f (io/as-file path)]
+    (when (.exists f)
+      (let [p (.getAbsolutePath f)]
+        (with-open [r (io/reader f)]
+          (mapv #(->env-file-event p (inc %2) %1)
+                (line-seq r)
+                (range)))))))
 
 (defn source-system-env
   [->sys-env-event]
